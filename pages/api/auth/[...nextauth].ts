9325059e84ba2b40fd5f3af37609ebde7ae44297
PankajK <pankajk.dev0000@gmail.com>
2024-05-14 23:14:03 +0530

    pre-deploy bug fixes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth/next"; //npm packages to install next-auth,bcrypt and @types/bcrypt
-import prismadb from "@/lib/prismadb"; //importing nextauth,prismadb from local lib getting compare from bcrypt and credentials from next-auth/providers/credentials
-import { compare } from "bcrypt"; //install next-auth/prisma-adapter
+import NextAuth, { AuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { AuthOptions } from "next-auth";
+
+import { compare } from "bcrypt";
+
+import prismadb from "@/lib/prismadb";
 
 export const authOptions: AuthOptions = {
   providers: [
-    //an array of authentication providers Google,Github and credentials
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "", //register the oAuth application at github too
+      clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
     }),
     GoogleProvider({
@@ -30,42 +30,41 @@ export const authOptions: AuthOptions = {
           label: "Password",
           type: "password",
         },
-        async authorize(credentials) {
-          //validating credentials
-          if (!credentials?.email || !credentials?.password) {
-            throw new Error("Email and Password is required");
-          }
-          const user = await prismadb.user.findUnique({
-            where: {
-              email: credentials.email,
-            },
-          });
-          if (!user || !user.hashedPassword) {
-            throw new Error("Email does not exist");
-          }
-          const isCorrectPassword = await compare(
-            credentials.password,
-            user.hashedPassword
-          );
-          if (!isCorrectPassword) {
-            throw new Error("Incorrect password");
-          }
-          return user;
-        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
+
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
       },
     }),
   ],
   pages: {
-    //an object that defines the page for authentication, here Sign In is set to /auth
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
-  //if env variable is set to development then debug mode will be on
-  adapter: PrismaAdapter(prismadb), //connecting Prisma adapter to pridma db
-  session: {
-    //object defining session strategy  here it is json web token
-    strategy: "jwt",
-  },
+  adapter: PrismaAdapter(prismadb),
+  session: { strategy: "jwt" },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
